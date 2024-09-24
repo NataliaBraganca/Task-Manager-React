b11f82c762d0318cd226d4ae0501ec8c2345368f
Natalia Eifländer Braganca <nataliabragancan@gmail.com>
2024-09-24 19:16:28 +0200

    onAddTaskSubmit

--- src/App.jsx
+++ src/App.jsx
@@ -41,20 +41,28 @@ function App() {
  }
 
  function onDeleteTaskClick(taskId) {
-
   const newTasksDelete = tasks.filter(task =>  task.id !== taskId)
-
   setTasks(newTasksDelete)
+ }
 
+ function onAddTaskSubmit (title, description) {
+  const newTask = {
+    id: tasks.length + 1,
+    title,
+    description,
+    isCompleted: false,
+  }
 
+  setTasks([...tasks,newTask]);
  }
 
   return (
     <div className= "w-screen h-screen bg-slate-500 flex justify-center p-6">
-        <div className= "w-[500px]">
+        <div className= "w-[500px] space-y-4">
         <h1 className="text-3xl text-slate-100 font-bold text-center">Task Manager</h1>
+        <AddTask onAddTaskSubmit={onAddTaskSubmit} />
         <Tasks tasks= {tasks} onTaskClick={onTaskClick} onDeleteTaskClick={onDeleteTaskClick}/>
-        <AddTask />
+
         </div>
     </div>
   );
--- src/components/AddTask.jsx
+++ src/components/AddTask.jsx
@@ -1,9 +1,32 @@
-import React from 'react'
+import React from "react";
 
-function AddTask() {
+function AddTask({onAddTaskSubmit}) {
+  const [title, setTitle] = React.useState("");
+  const [description, setDescription] = React.useState("");
+
   return (
-    <h1></h1>
-  )
+    <div className="space-y-4 p-6 bg-slate-200 rounded-md shadow flex flex-col">
+     <input type="text"
+      placeholder="Type the title of the task"
+      className= "border border-slate-300 outline-slate-400 px-4 py-2 rounded-md"
+      value = {title}
+      onChange={(event)=> setTitle(event.target.value)}
+      />
+
+     <input type="text"
+     placeholder="Type the description of the task"
+     className= "border border-slate-300 outline-slate-400 px-4 py-2 rounded-md"
+     value = {description}
+     onChange={(event)=> setDescription(event.target.value)}
+     />
+     <button
+     onClick={() => {
+      onAddTaskSubmit(title, description)
+      setTitle("")
+      setDescription("")}}
+     className="bg-slate-500 text-white px-4 py-2 rounded-md">Add task</button>
+    </div>
+  );
 }
 
 export default AddTask;
