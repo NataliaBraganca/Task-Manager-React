01f5ff7b67f7c9d7747732e2d769694c604d4493
Natalia Eifländer Braganca <nataliabragancan@gmail.com>
2024-09-26 12:18:05 +0200

    add button

--- src/pages/TaskPage.jsx
+++ src/pages/TaskPage.jsx
@@ -1,7 +1,9 @@
+import { ChevronsLeftIcon } from 'lucide-react';
 import React from 'react'
-import { useSearchParams } from 'react-router-dom'
+import { useNavigate, useSearchParams } from 'react-router-dom'
 
 function TaskPage() {
+  const navigate = useNavigate();
   const [searchParams] = useSearchParams();
   const title = searchParams.get("title");
   const description = searchParams.get("description");
@@ -9,13 +11,20 @@ function TaskPage() {
   return (
   <div className= "h-screen w-screen bg-slate-500 p-6">
     <div className= "w-[500px] space-y-4">
-      <h1 className="text-3xl text-slate-100 font-bold text-center">
-        Detalhe da tarefa
-      </h1>
+      <div className="flex justify-center relative mb-6">
+        <button
+        onClick={() => navigate(-1)}
+        className="absolute left-0 top-0 bottom-0 text-slate-100">
+          <ChevronsLeftIcon />
+        </button>
+        <h1 className="text-3xl text-slate-100 font-bold text-center">
+          Detalhe da tarefa
+        </h1>
+      </div>
 
-      <div className="bg-slate-400 p-4 rounded-md">
-        <h2 className="text-xl text-white font-bold">{title}</h2>
-        <p className="text-white">{description}</p>
+      <div className="bg-slate-200 p-4 rounded-md">
+        <h2 className="text-xl font-bold text-slate-600">{title}</h2>
+        <p className="text-slate-600">{description}</p>
       </div>
     </div>
   </div>
