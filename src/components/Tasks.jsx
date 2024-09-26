04e4e8ceeb7681df73df7ae659627f587a0f9117
Natalia Eifländer Braganca <nataliabragancan@gmail.com>
2024-09-26 14:58:15 +0200

    button component

--- src/components/Tasks.jsx
+++ src/components/Tasks.jsx
@@ -1,5 +1,6 @@
 import { ChevronsRightIcon, TrashIcon } from "lucide-react";
 import { useNavigate } from "react-router-dom";
+import Button from "./Button"
 
 function Tasks({tasks, onTaskClick, onDeleteTaskClick}) {
 
@@ -23,17 +24,13 @@ function Tasks({tasks, onTaskClick, onDeleteTaskClick}) {
           }`}
         >
             {task.title}
-
           </button>
-          <button onClick={()=> onSeeDetailsClick(task)}
-          className= "bg-slate-400 p-2 rounded-md text-white" >
+          <Button onClick={()=> onSeeDetailsClick(task)}>
             <ChevronsRightIcon />
-          </button>
-          <button
-          onClick={() => onDeleteTaskClick(task.id)}
-          className= "bg-slate-400 p-2 rounded-md text-white" >
+          </Button>
+          <Button onClick={() => onDeleteTaskClick(task.id)}>
             <TrashIcon />
-          </button>
+          </Button>
         </li>
       ))}
     </ul>
